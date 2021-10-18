a93e321c2b82687f51678fdf3f2bde29d51325dc
Sebyte <sebaponti@gmail.com>
2021-10-18 14:54:23 -0300

    [ADD] Init function to Vesting contratct

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -16,8 +16,9 @@ contract Vesting is Ownable, ReentrancyGuard {
     address public oracle;
     uint8 public lastCmcRank;
     uint256 public rankUdatedAt;
-    uint256 public initAt;
+    uint256 public totalVestingAmount;
     uint256 public scoringEpochSize = 60 days;
+    bool public initialized;
 
     struct LockVesting {
         uint256 totalAmount;
@@ -37,6 +38,16 @@ contract Vesting is Ownable, ReentrancyGuard {
         _;
     }
 
+    modifier notInitialized() {
+        require(!initialized, "contract instance has already been initialized");
+        _;
+    }
+
+    modifier isInitialized() {
+        require(initialized, "contract instance has not been initialized");
+        _;
+    }
+
     constructor(
         address _token,
         address _operator,
@@ -48,7 +59,36 @@ contract Vesting is Ownable, ReentrancyGuard {
         token = _token;
         operator = _operator;
         oracle = _oracle;
-        initAt = _currentTime();
+    }
+
+    function initialize() external onlyOperator notInitialized {
+        require(
+            totalVestingAmount == IERC20(token).balanceOf(address(this)),
+            "vesting amount and token balance are different"
+        );
+        initialized = true;
+        rankUdatedAt = _currentTime();
+    }
+
+    /// @notice This function it's executed by the operator and grants
+    /// the vesting for the beneficiary.
+    /// @param _beneficiary is the beneficiary address.
+    /// @param _amount the amount of tokens that will be locked in the vesting
+    function grantVesting(address _beneficiary, uint256 _amount)
+        external
+        onlyOperator
+        notInitialized
+    {
+        require(_amount != 0, "_amount is required");
+        require(_beneficiary != address(0), "beneficiary address is required");
+        require(
+            locks[_beneficiary].totalAmount == 0,
+            "beneficiary already has a vesting"
+        );
+
+        locks[_beneficiary].totalAmount = _amount;
+        beneficiaries.push(_beneficiary);
+        totalVestingAmount = totalVestingAmount + _amount;
     }
 
     function setOperator(address _operator) external onlyOwner {
@@ -61,7 +101,12 @@ contract Vesting is Ownable, ReentrancyGuard {
         oracle = _oracle;
     }
 
-    function updateRank(uint8 _newCmcRank) external onlyOracle nonReentrant {
+    function updateRank(uint8 _newCmcRank)
+        external
+        onlyOracle
+        nonReentrant
+        isInitialized
+    {
         require(
             rankUdatedAt + scoringEpochSize < _currentTime(),
             "scoring epoch still not finished"
@@ -83,25 +128,6 @@ contract Vesting is Ownable, ReentrancyGuard {
                 "token transfer fail"
             );
         }
-    }
-
-    /// @notice This function it's executed by the operator and grants
-    /// the vesting for the beneficiary.
-    /// @param _beneficiary is the beneficiary address.
-    /// @param _amount the amount of tokens that will be locked in the vesting
-    function grantVesting(address _beneficiary, uint256 _amount)
-        external
-        onlyOperator
-    {
-        require(_amount != 0, "_amount is required");
-        require(_beneficiary != address(0), "beneficiary address is required");
-        require(
-            locks[_beneficiary].totalAmount == 0,
-            "beneficiary already has a vesting"
-        );
-
-        locks[_beneficiary].totalAmount = _amount;
-        beneficiaries.push(_beneficiary);
     }
 
     /// @notice This function it's executed by the locks to check the
